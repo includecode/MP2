a5cef7197a8dcdae1e29a7fbdb3ff0d289820b76
Pavel TIAH <pavel.samene@gmail.com>
2020-04-08 16:38:46 +0200

    Project over :)
    Simulation, all implementations are ok:NON Periodic: round-robin, first-come-first-served, short-remaining-time-first, shortest-job-first
    
    PERIODIC: Earliest deadlin-first and Rate-monotonic

--- scheduler/sched.c
+++ scheduler/sched.c
@@ -26,47 +26,149 @@
 #include <time.h>
 #include <unistd.h>
 #include "sched.h"
+#include "task_description.h"
 
 /* --Scheduler random--*/
-tproc * randomscheduler(tlist * procs, tlist * ready, int * delta) {
+tproc *randomscheduler(tlist *procs, tlist *ready, int *delta)
+{
     int length = len(ready);
-    int selected = rand()%length;
-    tnode * p = ready->first;
-    for (int i=0; i < selected; i++) {
+    int selected = rand() % length;
+    tnode *p = ready->first;
+    for (int i = 0; i < selected; i++)
+    {
         p = p->next;
     }
-    *delta = rand()%p->proc->remaining + 1;
+    *delta = rand() % p->proc->remaining + 1;
     return p->proc;
 }
 /* --Scheduler random--*/
 
 /* --Scheduler fcfs-- */
-tproc * fcfs(tlist * procs, tlist * ready, int * delta) {
-    /* FIXME: Random scheduler, replace by appropriate code */
-    return randomscheduler(procs, ready, delta);
+tproc *fcfs(tlist *procs, tlist *ready, int *delta)
+{
+    /* take the first in the queue (first come)*/
+    tnode *p = ready->first;
+    *delta = 1;
+    return p->proc;
 }
 /* --Scheduler fcfs-- */
 
 /* --Scheduler rr-- */
-tproc * rr(tlist * procs, tlist * ready, int * delta) {
-    /* FIXME: Random scheduler, replace by appropriate code */
-    return randomscheduler(procs, ready, delta);
+tproc *rr(tlist *procs, tlist *ready, int *delta)
+{
+    tnode *p = ready->last;
+
+    //Select the last process at each turn (newest process)
+    //Send the last proc back to the head of the queue (for an eventual come back if not finished)
+    if (len(ready) > 1)
+    {
+        tnode *f = NULL;
+
+        for (f = ready->first; f->next != NULL; f = f->next)
+            ; //this loop just finds the last process in queue
+        f->next = ready->first;
+        ready->last = ready->first;
+        ready->first = ready->last->next;
+        ready->last->next = NULL;
+    }
+    *delta = 1;
+    return p->proc;
 }
 /* --Scheduler rr-- */
 
 /* --Scheduler sjf-- */
-tproc * sjf(tlist * procs, tlist * ready, int * delta) {
-    /* FIXME: Random scheduler, replace by appropriate code */
-    return randomscheduler(procs, ready, delta);
+tproc *sjf(tlist *procs, tlist *ready, int *delta)
+{
+    tnode *srt_process = ready->first;
+
+    //find the process which has the shortest lenght
+    for (tnode *p = srt_process->next; p != NULL; p = p->next)
+    {
+        if (p->proc->length < srt_process->proc->length)
+            srt_process = p;
+    }
+fprintf(stderr, " +++++l is %d proc = %d rem = %d\n", srt_process->proc->length, srt_process->proc->pid, srt_process->proc->remaining);
+    //Simulate execution of the same process untill the end
+    while (srt_process->proc->length > 0)
+    {
+        srt_process->proc->length--;
+        (*delta)++;
+    }
+fprintf(stderr, " +++++delta is %d proc = %d rem = %d\n", *delta, srt_process->proc->pid, srt_process->proc->remaining);
+
+    return srt_process->proc;
 }
 /* --Scheduler sjf-- */
 
 /* --Scheduler srtf-- */
-tproc * srtf(tlist * procs, tlist * ready, int * delta) {
-    /* FIXME: Random scheduler, replace by appropriate code */
-    return randomscheduler(procs, ready, delta);
+tproc *srtf(tlist *procs, tlist *ready, int *delta)
+{
+    tnode *srt_process = ready->first;
+
+    //find the process which has the shortest remaining time
+    for (tnode *p = srt_process->next; p != NULL; p = p->next)
+    {
+        if (p->proc->remaining < srt_process->proc->remaining)
+            srt_process = p;
+    }
+
+    *delta = 1;
+    return srt_process->proc;
 }
 /* --Scheduler srtf-- */
+
+/* --Scheduler edf-- */
+tproc *edf(tlist *procs, tlist *ready, int *delta)
+{
+
+    tnode *edf_process = ready->first;
+
+    //find the process which has the nearest deadline
+    for (tnode *p = edf_process->next; p != NULL; p = p->next)
+    {
+        if (p->proc->activation + p->proc->period < edf_process->proc->activation + edf_process->proc->period)
+            edf_process = p;
+        else
+        {
+            //If same activation time, take the shortest length
+            if (p->proc->activation + edf_process->proc->period == edf_process->proc->activation + edf_process->proc->period)
+            {
+                if (p->proc->length < edf_process->proc->length)
+                    edf_process = p;
+            }
+        }
+    }
+    *delta = 1;
+    return edf_process->proc;
+}
+/* --Scheduler edf-- */
+
+/* --Scheduler rm-- */
+tproc *rm(tlist *procs, tlist *ready, int *delta)
+{
+    tnode *rm_process = ready->first;
+
+    //find the process which has the highest priority
+    for (tnode *p = rm_process->next; p != NULL; p = p->next)
+    {
+        if ((float)1 / p->proc->period > (float)1 / rm_process->proc->period)
+        {
+            rm_process = p;
+        }
+        else
+        {
+            //If same period, take the shortest length
+            if ((float)1 / p->proc->period == (float)1 / rm_process->proc->period)
+            {
+                if (p->proc->length < rm_process->proc->length)
+                    rm_process = p;
+            }
+        }
+    }
+    *delta = 1;
+    return rm_process->proc;
+}
+/* --Scheduler rm-- */
 
 /* List of ready procs */
 tlist ready;
@@ -78,105 +180,176 @@ tlist procs;
 tscheduler scheduler;
 
 /* The scheduling statistics */
-tstats stats = {0} ;
+tstats stats = {0};
 
 /* display usage message */
-void usage() {
+void usage()
+{
     fail("usage: sched [fcfs, rr, sjf, srtf]\n");
 }
 
 /* simulate a single core scheduler, from time 0 to `max_time` */
-void simulate(int max_time) {
-    int time=0;
-    while(time <= max_time) {
+void simulate(int max_time)
+{
+    /* array to know when a process is being runned for the first time */
+    /* (0) ==> process has never been runned, or (1) ==> process had a previous run */
+    char *process_first_touch = malloc(sizeof(char) * len(&procs));
+    for (tnode *p = procs.first; p != NULL; p = p->next)
+        process_first_touch[p->proc->pid] = 0;
+
+    /* [PERIODIC] array to keep initial activation time of periodic processes */
+    char *initial_remainings = malloc(sizeof(char) * len(&procs));
+    for (tnode *p = procs.first; p != NULL; p = p->next)
+        initial_remainings[p->proc->pid] = p->proc->remaining;
+
+    int time = 0;
+    stats.waiting = 0;
+    stats.response = 0;
+    stats.completion = 0;
+    tnode *f = NULL;
+    for (f = procs.first; f != NULL; f = f->next)
+        stats.completion += f->proc->length;
+    while (time <= max_time)
+    {
+        int old_time = time;
         /* Activate process */
-        for (tnode * p = procs.first; p != NULL;) {
-            tproc * proc = p->proc; 
+        for (tnode *p = procs.first; p != NULL;)
+        {
+            tproc *proc = p->proc;
             p = p->next;
 
             /* Move every process which should be activated,
              * from the procs list to the ready list */
-            if (proc->activation <= time) {
+            if (proc->activation <= time)
+            {
                 del(&procs, proc);
                 add(&ready, proc);
+                //on sjf process are not added at the time they should be added beacause another
+                //process has running priority, so ad this to response time
+                stats.response += (time - proc->activation);
             }
         }
 
         /* If any process is ready, then we can schedule! */
-        if (ready.first != NULL) {
+        if (ready.first != NULL)
+        {
 
             int delta = 0;
             /* Call the scheduler */
-            tproc * proc = scheduler(&procs, &ready, &delta);
+            tproc *proc = scheduler(&procs, &ready, &delta);
+
+            /*Set process to taken */
+            process_first_touch[proc->pid] = 1;
 
             /* Ensure the scheduler has advanced at least one unit of time */
             assert(delta > 0);
 
             /* Output task execution */
-            printf("\\TaskExecution{%d}{%d}{%d}\n", proc->pid, time, time+delta);
+            /* red color for periodic tasks that dont meet the deadline */
+            if (time >= (proc->activation + proc->period) && proc->period > 0)
+                printf("\\TaskExecution[color=red]{%d}{%d}{%d}\n", proc->pid, time, time + delta);
+            else
+                printf("\\TaskExecution{%d}{%d}{%d}\n", proc->pid, time, time + delta);
 
             /* Advance time by delta */
             time += delta;
 
             /* Remove delta from chosen process */
-            proc->remaining-=delta;
- 
+            proc->remaining -= delta;
+
             /* If the process remaining time is less zero or less, 
-             * delete it */ 
-            if (proc->remaining <= 0) {
+             * delete it */
+            if (proc->remaining <= 0)
+            {
+                stats.completion += (time - (proc->activation + proc->length));
+                stats.waiting += (time - (proc->activation + proc->length));
                 del(&ready, proc);
                 del(&procs, proc);
+                if (proc->period > 0) // periodic task
+                {
+                    //Update & Send periodic tasks back to the list of processes
+                    proc->remaining = initial_remainings[proc->pid];
+                    proc->activation = proc->activation + proc->period;
+                    add(&procs, proc);
+                    printf("\\TaskArrival{%d}{%d}\n", proc->pid, proc->activation);
+                    printf("\\TaskDeadline{%d}{%d}\n", proc->pid, proc->activation);
+                }
             }
-        } 
+
+            /* Update response time for other processes */
+            for (tnode *p = ready.first; p != NULL; p = p->next)
+            {
+
+                if (process_first_touch[p->proc->pid] == 0)
+                    stats.response += (time - old_time);
+            }
+        }
         /* If no process is ready, just advance the simulation timer */
-        else {
+        else
+        {
             time += 1;
         }
-
     }
+    /* release memory */
+    free(process_first_touch);
+    free(initial_remainings);
 }
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[])
+{
 
     /* Parse arguments */
-    if (argc != 2) usage(); 
+    if (argc != 2)
+        usage();
 
     /* Seed random number generator */
     srand(time(NULL) ^ getpid());
 
-    char * method = argv[1]; 
+    char *method = argv[1];
 
     /* The sched argument should be one of fcfs, rr, sjf, srtf */
-    if (strcmp(method, "fcfs") == 0) {
+    if (strcmp(method, "fcfs") == 0)
+    {
         scheduler = fcfs;
-    } 
-    else if (strcmp(method, "rr") == 0) {
+    }
+    else if (strcmp(method, "rr") == 0)
+    {
         scheduler = rr;
     }
-    else if (strcmp(method, "sjf") == 0) {
+    else if (strcmp(method, "sjf") == 0)
+    {
         scheduler = sjf;
     }
-    else if (strcmp(method, "srtf") == 0) {
+    else if (strcmp(method, "srtf") == 0)
+    {
         scheduler = srtf;
-    } 
-    else {
+    }
+    else if (strcmp(method, "edf") == 0)
+    {
+        scheduler = edf;
+    }
+    else if (strcmp(method, "rm") == 0)
+    {
+        scheduler = rm;
+    }
+    else
+    {
         usage();
     }
 
-    /* Compile the task descriptions */
-    #include "task_description.h"
-
     /* Add all tasks to the procs queue */
-    for(int i = 0; i < sizeof(tasks)/sizeof(tproc); i ++) {
+    for (int i = 0; i < sizeof(tasks) / sizeof(tproc); i++)
+    {
         add(&procs, &(tasks[i]));
     }
-    
+
     /* Output RTGrid header */
     printf("\\begin{RTGrid}[width=0.8\\textwidth]{%d}{%d}\n", len(&procs), max_time);
 
-    /* Output task arrivals for all tasks */ 
-    for (tnode * p = procs.first; p != NULL; p = p->next) {
-        printf("\\TaskArrival{%d}{%d}\n", p->proc->pid, p->proc->activation); 
+    /* Output task arrivals for all tasks */
+    for (tnode *p = procs.first; p != NULL; p = p->next)
+    {
+        printf("\\TaskArrival{%d}{%d}\n", p->proc->pid, p->proc->activation);
     }
 
     /* Start scheduling simulation */
--- scheduler/task_description.h
+++ scheduler/task_description.h
@@ -1,8 +1,34 @@
-int max_time = 20;
+int max_time = 40;
 tproc tasks[] = {
-    {1,0,3,3},
-    {2,2,6,6},
-    {3,4,4,4},
-    {4,6,5,5},
-    {5,8,2,2}
+    //pid  //activation  //length  //remaining  //period
+    {1,     0,          1,          1,          3},
+    {2,     0,          1,          1,          5},
+    {3,     1,          1,          1,          2},
 };
+/*
+
+_______________________________________________________________________________________________________________________
+Question: Proposez et testez un système de tâche qui est ordonnançable avec EDF et qui n’est pas ordonnançable avec RM.
+-----------------------------------------------------------------------------------------------------------------------
+
+Ce système de tâche marche avec EDF mais pas avec RM pour les raisons suivantes:
+PROPRIETE:
+    On a U ==> utilisation CPU
+    Si U ≤ n.(2exp(1/n) − 1) alors il existe un ordonnancement satisfaisant toutes les échéances.
+
+    Or:
+    * U = 1/3 + 1/5 + 1/2 = 1.033
+    * n.(2exp(1/n) − 1) = 0.779
+     ____________
+    | CONCLUSION |
+
+    1.033 > 0.779 donc toutes les échéances en RM ne seront pas satisfaites
+    On constate d'ailleurs sur le graphe d'exécution que la tâche T2 ne respecte plus ses deadlines en RM après 
+    sa troisième échéance.
+
+    - de plus U > 1
+
+    - En plus, le théorème de la zone critique n'est pas respecté, les taches ne respectent pas leurs premières échéances
+        et ne respecteront problablement pas les échéances futures. Le phénomène est observé pour T2 en RM après 
+        sa troisième échéance.
+ */
